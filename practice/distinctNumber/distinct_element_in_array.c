b3b91e9dbb48a28fdb8d8e1863c321abf4c07574
pratikadh <31108938+pratikadh@users.noreply.github.com>
2018-01-21 10:54:03 +0530

    Add files via upload

---
+++ practice/distinctNumber/distinct_element_in_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n,x,c;
+    printf("Enter total numbers:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements:\t");
+        for(i=0;i<n;i++)
+        {
+            scanf("\n %d",&a[i]);
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                if(a[i]==a[j])
+                break;
+            }
+            if(i==j)
+            {
+                printf("%d\t",a[i]);
+            }
+        }
+    }
+
