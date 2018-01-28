795915bcfef54931983c63c555a41824fac52412
pratikadh <31108938+pratikadh@users.noreply.github.com>
2018-01-28 19:09:09 +0530

    array(distinct_elements_in_ascending_order).c

--- practice/distinctNumber/distinct_element_in_array.c
+++ practice/distinctNumber/distinct_element_in_array.c
@@ -1,26 +1,42 @@
+/*By Pratik :) */
 #include<stdio.h>
+#define MAX 100
 void main()
 {
-    int i,j,n,x,c;
-    printf("Enter total numbers:");
+    int i,j,temp,n,a[MAX];
+    printf("Enter the total size of the array:\n");
     scanf("%d",&n);
-    int a[n];
-    printf("Enter the elements:\n");
-        for(i=0;i<n;i++)
+    printf("Enter the elements in the array:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
         {
-            scanf("\n %d",&a[i]);
-        }
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<i;j++)
+            if(a[i]>a[j])
             {
-                if(a[i]==a[j])
-                break;
-            }
-            if(i==j)
-            {
-                printf("%d\t",a[i]);
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
             }
         }
     }
-
+    printf("The array in ascending order with distinct elements is:\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            if(a[i]==a[j])
+            {
+                break;
+            }
+        }
+        if(i==j)
+        {
+            printf("%d \t",a[i]);
+        }
+    }
+}
